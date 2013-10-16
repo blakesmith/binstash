a973405576acccaca5c4e2434e22906aad8d463f
Blake Smith <blakesmith0@gmail.com>
2013-10-16 08:51:47 -0500

    Execute the client function inside the Reader monad

--- Main.hs
+++ Main.hs
@@ -1,12 +1,19 @@
 import Binstash.Configuration
 import Binstash.Args
+import Control.Monad.Reader
 
 data Client = Client { _args :: Args
                      , _creds :: Credentials
                      } deriving (Show, Eq)
 
+type ClientEnv = ReaderT Client IO
+
+runClient :: ClientEnv ()
+runClient = return ()
+
 main :: IO ()
 main = do
      args <- getArgs
      creds <- getCredentials
-     putStrLn . show $ Client args creds
+     result <- runReaderT runClient (Client args creds)
+     putStrLn . show $ result
