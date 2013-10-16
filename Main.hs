2ea7b06a9f8ec331e0985f44b84166ab93ac5423
Blake Smith <blakesmith0@gmail.com>
2013-10-15 22:40:36 -0500

    Wrap all client data in its own type

--- Main.hs
+++ Main.hs
@@ -1,7 +1,12 @@
 import Binstash.Configuration
 import Binstash.Args
 
-main :: IO ()
--- main = getCredentials >>= (putStrLn . show)
+data Client = Client { _args :: Args
+                     , _creds :: Credentials
+                     } deriving (Show, Eq)
 
-main = getArgs >>= print
+main :: IO ()
+main = do
+     args <- getArgs
+     creds <- getCredentials
+     putStrLn . show $ Client args creds
