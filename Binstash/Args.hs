83103adcafbb64604e1bef129f46f14dc4b57c4f
Blake Smith <blakesmith0@gmail.com>
2013-10-16 10:03:33 -0500

    Send version

--- Binstash/Args.hs
+++ Binstash/Args.hs
@@ -4,10 +4,13 @@ import System.Console.CmdArgs
 
 data Args = Args { command :: String } deriving (Data, Typeable, Show, Eq)
 
+version :: String
+version = "0.0.1"
+
 binArgs :: Args 
 binArgs = Args { command = def &= args &= typ "COMMAND"}
         &= program "binstash"
-        &= summary "BinStash command line client"
+        &= summary ("BinStash command line client (" ++ version ++ ")")
 
 getArgs :: IO Args
 getArgs = cmdArgs binArgs
