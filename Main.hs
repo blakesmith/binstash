1c88d0d34add84d935104ad9ac0449d8f53e1fdf
Blake Smith <blakesmith0@gmail.com>
2013-10-20 23:01:21 -0500

    Simplify main function

--- Main.hs
+++ Main.hs
@@ -1,10 +1,7 @@
+import Control.Applicative (liftA2)
 import Binstash.Args
 import Binstash.Cmd
 import Binstash.Configuration
 
 main :: IO ()
-main = do
-     args <- getArgs
-     creds <- getCredentials
-     result <- run $ Client args creds
-     putStrLn result
+main = liftA2 Client getArgs getCredentials >>= run >>= putStrLn
