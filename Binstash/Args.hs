af989c1969aa9a750a01309ea814cd16256329fd
Blake Smith <blakesmith0@gmail.com>
2013-10-21 10:19:54 -0500

    Default domain description

--- Binstash/Args.hs
+++ Binstash/Args.hs
@@ -18,7 +18,7 @@ binArgs = Args { command = def &= args &= typ "COMMAND"
                , filename = def &= typFile &= help "Package file to upload"
                , directory_ = def &= name "d" &= typ "DIRECTORY" &= help "Package directory. Usually your username"
                , name_ = def &= typ "NAME" &= help "Repository name, run 'binstash list' to see your repositories"
-               , domain_ = "api.binstash.com" &= typ "DOMAIN" &= help "Binstash API domain. Defaults to 'http://'"
+               , domain_ = "api.binstash.com" &= typ "DOMAIN" &= help "Binstash API domain. Defaults to 'api.binstash.com'"
                , secure_ = False &= typ "SECURE" &= help "Should the client use https?"
                }
         &= program "binstash"
